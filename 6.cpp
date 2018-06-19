31bd2e7ae7e432b29a8dd12405b66c4766f7c0d8
gravimetrix <40379715+gravimetrix@users.noreply.github.com>
2018-06-19 11:26:47 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+#include<iostream>
+#include<iomanip>
+int main()
+{
+    int n;
+    cin>>n;
+    int result,i;
+    if(n>=2&&n<=20)
+        for(i=1;i<11;i++)
+        {
+            result=n*i;
+            cout<<n<<" x "<<i<<" = "<<result<<endl;
+        }
+        return 0;
+}
